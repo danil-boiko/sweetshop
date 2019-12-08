0150d30028ee51d6acd11f38b6c9e5bb45e68ed4
Daniil <boiko.81@list.ru>
2019-12-08 21:44:52 +0300

    add models of Category and Product and migrations for they

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -39,17 +39,9 @@
 
 <div class="container">
     <div class="starter-template">
-        <h1>
-            @if($category == 'mobile')
-                Мобильныe телефоны
-                @elseif($category == 'portable')
-                    Портативная техника
-                @elseif($category == 'appliances')
-                    Бытовая техника
-            @endif
-        </h1>
+        <h1>{{ $category->name }}</h1>
         <p>
-            В этом разделе вы найдёте самые популярные мобильные телефонамы по отличным ценам!
+            {{ $category->description }}
         </p>
         <div class="row">
             <div class="col-sm-6 col-md-4">
